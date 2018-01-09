284ad0423ab025bd1f0d52a9228828f2a0b6f1c9
Mateo Navarro <pedro-luis.mateo-navarro@capgemini.com>
2018-01-09 16:27:10 +0100

    updating SD for java

--- java/SuperDriver.java
+++ java/SuperDriver.java
@@ -1,97 +1,163 @@
-package unitest;
+package stepDefinition;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ById;
 import org.openqa.selenium.By.ByTagName;
 import org.openqa.selenium.By.ByXPath;
-import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 public class SuperDriver {
 
-	//Clase Superdriver que aumenta las funcionalidades del driver de Selenium
+	// Clase Superdriver que aumenta las funcionalidades del driver de Selenium
 	private RemoteWebDriver _driver;
 	private static final int WAIT_TIMEOUT = 10;
 
 	public SuperDriver(RemoteWebDriver driver) {
 
 		_driver = driver;
-		//Constructor que recibe como argumento el driver y crea un objeto de tipo SuperDriver
+		// Constructor que recibe como argumento el driver y crea un objeto de tipo
+		// SuperDriver
 	}
+	
+	///
+	/// Wait and get
+	///
 
-	public WebElement waitAndGet (By how) {
+	public WebElement waitAndGet(By how) {
 		try {
-			//return WebDriverWait(self._driver, self.WAIT_TIMEOUT).until(EC.visibility_of_element_located((mode, key)))
+			// return WebDriverWait(self._driver,
+			// self.WAIT_TIMEOUT).until(EC.visibility_of_element_located((mode, key)))
 
-			WebDriverWait wait = new WebDriverWait(_driver,WAIT_TIMEOUT);
-			wait.until(ExpectedConditions.presenceOfElementLocated(how) ); //throws a timeout exception if element not present after waiting <timeoutInSeconds> seconds
+			WebDriverWait wait = new WebDriverWait(_driver, WAIT_TIMEOUT);
+			wait.until(ExpectedConditions.presenceOfElementLocated(how)); // throws a timeout exception if element not
+			// present after waiting <timeoutInSeconds>
+			// seconds
 			return _driver.findElement(how);
-		}
-		catch (Exception e){
-		
-		return null;	
+		} catch (Exception e) {
+
+			return null;
 
 		}
 	}
-	//Usando los metodos del driver, busca un elemento (key) mediante un patron (how)
-	//Todos los metodos devuelven un objeto de tipo WebElement
+	// Usando los metodos del driver, busca un elemento (key) mediante un patron (how)
+	// Todos los metodos devuelven un objeto de tipo WebElement
 
-
-	public WebElement waitAndGetByXpath (String key) {
+	public WebElement waitAndGetByXpath(String key) {
 
 		ByXPath objetoBusqueda = new ByXPath(key);
 		return waitAndGet(objetoBusqueda);
 
-		//Usando los metodos del driver, busca un elemento por XPATH (key) y lo devuelve.
+		// Usando los metodos del driver, busca un elemento por XPATH (key) y lo
+		// devuelve.
 
 	}
 
-	public WebElement waitAndGetById (String key) {
+	public WebElement waitAndGetById(String key) {
 		ById objetoBusqueda = new ById(key);
 		return waitAndGet(objetoBusqueda);
 
-		//Usando los metodos del driver, busca un elemento por ID (key) y lo devuelve.
+		// Usando los metodos del driver, busca un elemento por ID (key) y lo devuelve.
 
 	}
-	public WebElement waitAndGetByTagName (String key) {
+
+	public WebElement waitAndGetByTagName(String key) {
 
 		ByTagName objetoBusqueda = new ByTagName(key);
 		return waitAndGet(objetoBusqueda);
-
-		//Usando los metodos del driver, busca un elemento por XPATH (key) y lo devuelve.
+		// Usando los metodos del driver, busca un elemento por XPATH (key) y lo
+		// devuelve.
 
 	}
 
+	///
+	/// Wait and Click
+	///
 
-	public void waitAndClick(String mode, String key) {
-		String uppmode = mode.toUpperCase();
+	public void waitAndClick(How mode, String key) {
+
 		WebElement elemento;
-		if (uppmode =="XPATH") {
-			elemento = waitAndGetByXpath(key); 
-			elemento.click();}
-		if (uppmode =="ID") {
-			elemento = waitAndGetById(key); 
-			elemento.click();}
-		if (uppmode =="TAGNAME") {
+		if (mode == How.XPATH) {
+			elemento = waitAndGetByXpath(key);
+			elemento.click();
+		}
+		if (mode == How.ID) {
+			elemento = waitAndGetById(key);
+			elemento.click();
+		}
+		if (mode == How.TAG_NAME) {
 			elemento = waitAndGetByTagName(key);
-			elemento.click();}
+			elemento.click();
+		}
 	}
-
 
 	public void waitAndClickByXpath(String key) {
-		waitAndClick("XPATH", key);
+		waitAndClick(How.XPATH, key);
 	}
 
 	public void waitAndClickByID(String key) {
-		waitAndClick("ID", key);
+		waitAndClick(How.ID, key);
 	}
+
 	public void waitAndClickByTagName(String key) {
-		waitAndClick("TAGNAME", key);
+		waitAndClick(How.TAG_NAME, key);
 	}
 
+	///
+	/// Wait and Select option
+	///
 
+	public void waitAndSelectOption(How mode, String key, int option) {
+
+		WebElement elemento;
+
+		if (mode == How.XPATH) {
+			elemento = waitAndGetByXpath(key);
+			Select sel = new Select(elemento);
+			sel.selectByIndex(option);
+		}
+		if (mode == How.ID) {
+			elemento = waitAndGetById(key);
+			Select sel = new Select(elemento);
+			sel.selectByIndex(option);
+		}
+		if (mode == How.TAG_NAME) {
+			elemento = waitAndGetByTagName(key);
+			Select sel = new Select(elemento);
+			sel.selectByIndex(option);
+		}
+	}
+
+	public void waitAndSelectOptionByID(String key, int option) {
+		waitAndSelectOption(How.ID, key, option);
+	}
+
+	///
+	/// wait and sendkey
+	///
+
+	private void waitAndSendKeys(How mode, String key, String sendKey) {
+		WebElement elemento;
+		if (mode == How.XPATH) {
+			elemento = waitAndGetByXpath(key);
+			elemento.sendKeys(sendKey);
+		}
+		if (mode == How.ID) {
+			elemento = waitAndGetById(key);
+			elemento.sendKeys(sendKey);
+		}
+		if (mode == How.TAG_NAME) {
+			elemento = waitAndGetByTagName(key);
+			elemento.sendKeys(sendKey);
+		}
+	}
+
+	public void waitAndSendKeysByID(String key, String sendKey) {
+		waitAndSendKeys(How.ID, key, sendKey);
+	}
 
 }
